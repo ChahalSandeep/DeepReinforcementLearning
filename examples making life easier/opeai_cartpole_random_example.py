46507ae066a2a464257506914456742778ffde47
sandeepchahal <chahal.sdp@gmail.com>
2024-08-19 10:55:29 -0500

    printing actions now

--- examples making life easier/opeai_cartpole_random_example.py
+++ examples making life easier/opeai_cartpole_random_example.py
@@ -10,15 +10,18 @@ if __name__ == '__main__':
     total_reward = 0
     total_steps = 0
     obs = env.reset()
+    list_of_actions = []
     while True:
         action = env.action_space.sample()
         obs_, reward, done, _, info = env.step(action) #  obs reward termination truncated information
         total_reward += reward
         total_steps += 1
+        list_of_actions.append(action)
         if done:
             break
 
     print("Episode done in %d steps and total reward is %.4f" % (total_steps, total_reward))
+    print("actions: ", list_of_actions)
     env.close()
 
 
