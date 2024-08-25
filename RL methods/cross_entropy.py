1dbef3d4830abc4cbd2b8efce2b76c47c113b6cc
sandeepchahal <chahal.sdp@gmail.com>
2024-08-24 20:17:10 -0500

    solved episode initial observation, uses long for loss function, added recording

--- RL methods/cross_entropy.py
+++ RL methods/cross_entropy.py
@@ -22,6 +22,7 @@ from collections import namedtuple
 # third-party packages
 from torch import nn
 from torch import FloatTensor
+from torch import LongTensor
 import numpy as np
 import gymnasium as gym
 import torch.optim as optim
@@ -34,6 +35,7 @@ from nn_archs import CrossEntropyNeuralNet as CrossNet
 HIDDEN_SIZE = 128
 BATCH_SIZE = 16
 PERCENTILE_TO_SELECT = 70
+REWARD_THRESHOLD = 999
 
 
 # defining helper classes using collection
@@ -59,7 +61,8 @@ def iterate_batches(env, net, batch_size):
     g_batch = [] # accumulates batch (list of episode instances)
     episode_rewards = 0.0 # reward for current episode
     episode_steps = [] # lost of steps in episode
-    obs = env.reset() # reset observation to first observation
+    obs_initial = env.reset() # reset observation to first observation
+    obs = obs_initial[0] # also gives a dictionary with observation
     g_softmax = nn.Softmax(dim=1) # used to convert networks output to probabilities of actions
 
     # at every iteration we convert observation to pytorch tensor and pass to network
@@ -86,11 +89,11 @@ def iterate_batches(env, net, batch_size):
             # reset environment and episodic variables
             episode_rewards = 0.0
             episode_steps = []
-            next_obs = env.reset()
+            next_obs = env.reset()[0]  # gets rid of dictionary in the tuple
             # if batch has reached desired episode count return it to caller for processing using yield
             if len(g_batch) == batch_size:
                 # generator function control is transferred to outer iteration loop and then continued after yield
-                yield g_batch
+                yield g_batch  # runs for 16 episodes
                 # clean up batch
                 g_batch = []
 
@@ -136,7 +139,7 @@ def filter_batch(g_batch, percentile):
 
 
 def main():
-    env = gym.make('CartPole-v0') # create env
+    env = gym.make('CartPole-v0', render_mode="rgb_array") # create env
     obs_size = env.observation_space.shape[0] # get observation size
     n_actions = env.action_space.n # get number of actions
     net = CrossNet(obs_size, HIDDEN_SIZE , n_actions) # object for our nn
@@ -144,21 +147,30 @@ def main():
     optimizer =optim.Adam(params=net.parameters(), lr=0.001) # define optimizer
     writer = SummaryWriter('logs/') # initialize writer
 
+    env = gym.wrappers.RecordVideo(env, "recording")
+    env.start_recording("cartpole_video_game")
+
     for iter_no, batch in enumerate(iterate_batches(env, net, BATCH_SIZE)):
         obs_v, acts_v, reward_b, reward_m = filter_batch(batch, PERCENTILE_TO_SELECT)
         optimizer.zero_grad()
         action_scores_v = net(obs_v)
-        loss_v = obj(action_scores_v, acts_v)
+        loss_v = obj(action_scores_v, acts_v.type(dtype=LongTensor)) # torch function has issues with float type
         loss_v.backward()
         optimizer.step()
         print("%d: loss=%.3f, reward_mean=%.1f, rw_bound=%.1f" % (iter_no, loss_v.item(), reward_m, reward_b))
         writer.add_scalar('loss', loss_v.item(), iter_no)
         writer.add_scalar("reward_bound", reward_b, iter_no)
         writer.add_scalar("reward_mean", reward_m, iter_no)
-        if reward_m > 199:
+        if reward_m > REWARD_THRESHOLD:
             print("Solved!")
+            env.stop_recording()
+            env.close()
+            env.env.close()
             break
     writer.close()
+    # env.stop_recording()
+    # env.close()
+    # env.env.close()
 
 
 if __name__ == '__main__':
